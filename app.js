8ae0163d127236496d91736c3d1c0f779ae857ff
ripudsrathore <ripudamanrathore743@gmail.com>
2024-02-06 12:53:39 +0530

    ripud's_changes_602

--- app.js
+++ app.js
@@ -108,3 +108,34 @@ gsap.to(".second-smallimg", {
     y: -250,
     dureation: 5,
   });
+
+
+
+  let items = gsap.utils.toArray(".items"),
+  pageWrapper = document.querySelector(".page-wrapper");
+
+items.forEach((container, i) => {
+let localItems = container.querySelectorAll(".swapper-item"),
+    distance = () => {
+      let lastItemBounds = localItems[localItems.length-1].getBoundingClientRect(),
+          containerBounds = container.getBoundingClientRect();
+      return Math.max(0, lastItemBounds.right - containerBounds.right);
+    };
+gsap.to(container, {
+  x: () => -distance(), // make sure it dynamically calculates things so that it adjusts to resizes
+  ease: "none",
+  scrollTrigger: {
+    trigger: container,
+    start: "top top",
+    pinnedContainer: pageWrapper,
+   
+    pin: pageWrapper,
+    scrub: true,
+    invalidateOnRefresh: true // will recalculate any function-based tween values on resize/refresh (making it responsive)
+  }
+})
+});
+
+
+
+// swiper end
