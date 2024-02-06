90b41be65b438ef253b45ef0cb59c4f1ddb1a4f0
anishaagarwal2002 <anishaagrawal2002@gmail.com>
2024-02-06 11:30:44 +0530

    anisha_6/2/24_js

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
+
+
 gsap.registerPlugin(ScrollTrigger);
+
+gsap.registerPlugin(ScrollToPlugin);
+const scrollButton = document.querySelector("#scroll-button");
+const sectionCases = document.querySelector("#scroll-funt");
+scrollButton.addEventListener("click", () => {
+  console.warn(sectionCases);
+  gsap.to(window, {
+    duration: 1,
+    scrollTo: sectionCases,
+  });
+});
 
 // Rolling-Text-stat
 let direction = 1; // 1 = forward, -1 = backward scroll
